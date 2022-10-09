e876e460899aa0e86b00e2e5ed6b199466925f67
Christian Obinna Iheacho <iheachochristian@yahoo.com>
2022-10-09 13:54:39 +0100

    Upload Image to Firebase Storage

--- src/components/adminPanel/addProducts/AddProduct.js
+++ src/components/adminPanel/addProducts/AddProduct.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5925eee2fe267776976e252685c9f3e6b7efb6170b7161c2b789cc4d56a80b61
-size 2714
+oid sha256:144e5b9c31c414681cc668dc3e92e61f226d96121dfe96cc0c3d457d34abf309
+size 3282
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3551565ec719b0bc86ec4d6c5c03e10379b862f0cf01daabcfac4dfb63ccd43
-size 4898
+oid sha256:197adb6f239edc7eaa014d2f66a5755932e610d273feacaa1a2d7c203f0e588b
+size 4944
