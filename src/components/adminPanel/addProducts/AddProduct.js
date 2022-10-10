dff37211a1be132f2c48b0c483b37b3bbc558318
Christian Obinna Iheacho <iheachochristian@yahoo.com>
2022-10-10 02:05:05 +0100

    display upload progress

--- src/components/adminPanel/addProducts/AddProduct.js
+++ src/components/adminPanel/addProducts/AddProduct.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:144e5b9c31c414681cc668dc3e92e61f226d96121dfe96cc0c3d457d34abf309
-size 3282
+oid sha256:416d98ebfaab26ae611efbfcaf95834b476d554c85266b8305ab421da8a1f4d0
+size 4124
