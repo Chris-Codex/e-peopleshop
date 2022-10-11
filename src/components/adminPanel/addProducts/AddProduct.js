e1615c9930505e3c1a31ede260290493736339de
Christian Obinna Iheacho <iheachochristian@yahoo.com>
2022-10-11 08:25:31 +0100

    Upload products to firestore

--- src/components/adminPanel/addProducts/AddProduct.js
+++ src/components/adminPanel/addProducts/AddProduct.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:416d98ebfaab26ae611efbfcaf95834b476d554c85266b8305ab421da8a1f4d0
-size 4124
+oid sha256:577cf26de22f3bd5bb02c12fa4912030935a29dac55c45ce386749911fd0ea44
+size 5009
